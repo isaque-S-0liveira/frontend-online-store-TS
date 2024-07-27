e84bd441f43a5024c1b0c343338b721b68d7fb1b
Isaque Oliveira <zazac3179@gmail.com>
2024-07-27 14:55:15 -0300

    feat: botão de pesquisa no Header

--- src/components/Header/HeaderInput.tsx
+++ src/components/Header/HeaderInput.tsx
@@ -1,10 +1,38 @@
+import { useContext, useState } from 'react';
 import './HeaderInput.css';
+import SearchTermContext from '../../context/SearchProductsContext';
 
 function HeaderInput() {
+  const [search, setSearch] = useState('');
+  const { setSearchTermCT } = useContext(SearchTermContext);
+  const handleSearch = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSearch((e.target as HTMLInputElement).value);
+  };
+
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    setSearchTermCT(search);
+    setSearch('');
+  };
+
   return (
-    <form id="main-header-input">
-      <input className="form-control" type="text" placeholder="Digite o que você busca" />
-      <i className="bi bi-search" />
+    <form onSubmit={ handleSubmit } id="main-header-input">
+      <input
+        id="search-input"
+        className="form-control"
+        type="text"
+        placeholder="Digite o que você busca"
+        onChange={ handleSearch }
+        value={ search }
+      />
+      <button
+        id="header-search-button"
+        type="submit"
+        aria-label="Search"
+      >
+        <i className="bi bi-search" />
+      </button>
     </form>
   );
 }
