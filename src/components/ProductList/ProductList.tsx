8c5ec1f6f9d020f9742f38784bd81fd4888fb163
Isaque Oliveira <zazac3179@gmail.com>
2024-07-27 14:59:49 -0300

    feat: Renderização de produtos buscados por termos

---
+++ src/components/ProductList/ProductList.tsx
@@ -0,0 +1,88 @@
+import { useContext, useEffect, useState } from 'react';
+import SearchTermContext from '../../context/SearchProductsContext';
+import './ProductList.css';
+import { getProductsFromCategoryAndQuery } from '../../services/api';
+import { Product } from '../../types/ProductTypes';
+import Loading from '../Loading/Loading';
+
+function ProductList() {
+  const { searchTermCT, searchCategoryCT } = useContext(SearchTermContext);
+  const [isLoading, setIsLoading] = useState(true);
+  const [products, setProducts] = useState<Product[]>([]);
+  const [productsStatus, setProductStatus] = useState(
+    'Você ainda não realizou nenhuma busca',
+  );
+  const [productStatusAUX, setProductStatusAUX] = useState(
+    'Digite algum termo de pesquisa ou escolha uma categoria',
+  );
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        setIsLoading(true);
+        const response = await getProductsFromCategoryAndQuery(
+          searchCategoryCT,
+          searchTermCT,
+        );
+
+        setProducts(response);
+        if (searchTermCT !== '' && response.length === 0) {
+          setProductStatus(`Nenhum produto com o nome ${searchTermCT}  encontrado`);
+          setProductStatusAUX('Por favor, tente novamente');
+        }
+      } catch (error) {
+        console.log(error);
+
+        setProductStatus('Erro ao buscar produtos');
+        setProductStatusAUX(
+          'Por favor, revise sua conexão e termo procurado, depois tente novamente',
+        );
+      }
+      setIsLoading(false);
+    };
+
+    fetchProducts();
+  }, [searchTermCT, searchCategoryCT]);
+
+  if (isLoading) {
+    return (
+      <Loading />
+    );
+  }
+
+  if (products.length === 0 || products === undefined) {
+    return (
+      <div id="product-status-main">
+        <p id="product-status">{productsStatus}</p>
+        <p id="product-status-aux">{productStatusAUX}</p>
+      </div>
+    );
+  }
+  if (products !== undefined && products.length > 0) {
+    return (
+      <div id="product-list-main" className="row g-3">
+        {products.map((product) => (
+          <div key={ product.id } className="col-12 col-sm-4">
+            <div className="product-card">
+              <img src={ product.thumbnail } alt={ product.title } />
+              <p className="product-name">{product.title}</p>
+              <p className="product-price">
+                R$
+                {' '}
+                {product.price}
+              </p>
+              <button
+                className="primary-button"
+                type="button"
+              >
+                Adicionar ao carrinho
+              </button>
+            </div>
+          </div>
+        ))}
+      </div>
+    );
+  }
+}
+
+export default ProductList;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,17 @@
 import './Home.css';
 import SMCategories from './SMCategories/SMCategories';
 import LGCategories from './LGCategories/LGCategories';
+import ProductList from '../../components/ProductList/ProductList';
 
 function Home() {
   return (
-    <main className="main-content row" id="home-main">
+    <main className="main-content row " id="home-main">
       <SMCategories />
-      <div className="col-4 h-100">
+      <div className="col-lg-3 col-xxl-2 h-100 d-none d-lg-block p-0">
         <LGCategories />
+      </div>
+      <div className="col-12 col-lg-9 col-xxl-10 h-100">
+        <ProductList />
       </div>
     </main>
   );
