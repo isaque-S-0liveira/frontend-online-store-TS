ec71264138cff08b059eb4cb44cb6fd9ad90f207
Isaque Oliveira <zazac3179@gmail.com>
2024-07-27 14:57:33 -0300

    refactor: Refatoração na busca da api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,11 +1,28 @@
-import { Category } from '../types/ProductTypes';
+import { Category, Product } from '../types/ProductTypes';
 
 export async function getCategories(): Promise<Category[]> {
   const response = await fetch('https://api.mercadolibre.com/sites/MLB/categories');
   return response.json();
 }
 
-export async function getProductsFromCategoryAndQuery(categoryId: string, query: string) {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?category=${categoryId}&q=${query}`);
-  return response.json();
+export async function getProductsFromCategoryAndQuery(
+  categoryId: string,
+  query: string,
+): Promise<Product[]> {
+  let url = 'https://api.mercadolibre.com/sites/MLB/search?';
+
+  if (categoryId && query) {
+    url += `category=${categoryId}&q=${query}`;
+  } else if (categoryId) {
+    url += `category=${categoryId}`;
+  } else if (query) {
+    url += `q=${query}`;
+  }
+
+  const response = await fetch(url);
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  }
+  const data = await response.json();
+  return data.results as Product[];
 }
--- src/types/ProductTypes.ts
+++ src/types/ProductTypes.ts
@@ -2,3 +2,10 @@ export type Category = {
   id: string;
   name: string;
 };
+
+export type Product = {
+  id: string;
+  title: string;
+  price: number;
+  thumbnail: string;
+};
