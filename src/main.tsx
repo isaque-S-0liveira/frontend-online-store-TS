60fd7ebcc8cbd33e864422cbb69e760e02eafb10
Isaque Oliveira <zazac3179@gmail.com>
2024-08-01 13:27:05 -0300

    feat: add StrictMode to the main application

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,17 @@
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
 import App from './App.tsx';
 import './index.css';
 import SearchProductsProvider from './context/SearchProductsProvider.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <SearchProductsProvider>
-    <BrowserRouter basename="/frontend-online-store-TS">
-      <App />
-    </BrowserRouter>
-    ,
-  </SearchProductsProvider>,
+  <React.StrictMode>
+    <SearchProductsProvider>
+      <BrowserRouter basename="/frontend-online-store-TS">
+        <App />
+      </BrowserRouter>
+    </SearchProductsProvider>
+  </React.StrictMode>,
+
 );
