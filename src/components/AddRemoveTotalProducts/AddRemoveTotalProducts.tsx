1d32a792cf84d35ae9ad97afd0d5441cf82403e4
Isaque Oliveira <zazac3179@gmail.com>
2024-08-05 18:30:25 -0300

    feat: os botões de adicionar e remover do total de produtos manipulam a quantidade no localStore

--- src/components/AddRemoveTotalProducts/AddRemoveTotalProducts.tsx
+++ src/components/AddRemoveTotalProducts/AddRemoveTotalProducts.tsx
@@ -1,46 +1,100 @@
-import { useState } from 'react';
-import { Product } from '../../types/ProductTypes';
+/* eslint-disable react/require-default-props */
+import { useEffect, useState } from 'react';
+import { Product, ProductCart } from '../../types/ProductTypes';
 import './AddRemoveTotalProducts.css';
 import ValueFormatter from '../../services/ValueFormatter';
+import useStorageUpdate from '../../hooks/useStorageUpdate';
+import {
+  dispatchStorageUpdate,
+  getStoredProducts,
+  setStoredProducts } from '../../utils/storage';
 
 type AddRemoveTotalProductsProps = {
-  price: Product['price'];
-  setQuantityAndValue: React.Dispatch<React.SetStateAction<{
-    quantity: number;
-    value: number;
+  productId?: Product['id'];
+  inCart?: boolean;
+  price: ProductCart['price'];
+  totalValue?: ProductCart['totalPrice'];
+  productQuantity?: ProductCart['quantity'];
+  setQuantityAndValue?: React.Dispatch<React.SetStateAction<{
+    totalValue: ProductCart['totalPrice'];
+    quantity: ProductCart['quantity'];
   }>>;
 };
 
 function AddRemoveTotalProducts({
+  totalValue = 0,
+  productId,
+  inCart = false,
+  productQuantity = 1,
   price,
-  setQuantityAndValue }: AddRemoveTotalProductsProps) {
-  const [state, setState] = useState({ totalProducts: 1, amount: price });
+  setQuantityAndValue,
+}: AddRemoveTotalProductsProps) {
+  const [state, setState] = useState({
+    totalProducts: productQuantity, amount: productQuantity * price });
 
   const updateState = (newTotalProducts: number, newAmount: number) => {
     setState({ totalProducts: newTotalProducts, amount: newAmount });
-    setQuantityAndValue({ quantity: newTotalProducts, value: newAmount });
+    if (setQuantityAndValue) {
+      setQuantityAndValue({ quantity: newTotalProducts, totalValue: newAmount });
+    }
+  };
+
+  // Foi feito com o intuito remover e adicionar na quantidade e valor total quando o produto já estiver no carrinho e o usuario interagr com os botões + ou -
+  const updateLocalStorage = (quantity: number, totalPrice: number) => {
+    const storedProducts = getStoredProducts();
+    if (storedProducts) {
+      const productStorage = storedProducts.find(
+        (product) => product.id === productId,
+      );
+      if (productStorage) {
+        productStorage.quantity = quantity;
+        productStorage.totalPrice = totalPrice;
+      }
+      setStoredProducts(storedProducts);
+    }
+  };
+
+  // Foi feito com o intuito acresentar na quantidade e valor total quando o produto já estiver no carrinho e o usuário adicionar mais produtos por meio do botão addCart
+
+  const handleStorageUpdate = () => {
+    const storedProducts = getStoredProducts();
+    if (storedProducts) {
+      const productStorage = storedProducts.find(
+        (product) => product.id === productId,
+      );
+      if (productStorage) {
+        updateState(productStorage.quantity, productStorage.totalPrice);
+      }
+    }
   };
 
   const handleAddProduct = () => {
     const newTotalProducts = state.totalProducts + 1;
     const newAmount = state.amount + price;
     updateState(newTotalProducts, newAmount);
+    updateLocalStorage(newTotalProducts, newAmount);
   };
 
   const handleRemoveProduct = () => {
     if (state.totalProducts <= 1) return;
-
     const newTotalProducts = state.totalProducts - 1;
     const newAmount = state.amount - price;
     updateState(newTotalProducts, newAmount);
+    updateLocalStorage(newTotalProducts, newAmount);
   };
+
+  useEffect(() => {
+    dispatchStorageUpdate();
+  }, [state.totalProducts]);
+
+  useStorageUpdate(handleStorageUpdate);
 
   return (
     <div id="add-remove-total-products-container">
       <div id="total-value-products-container">
         <span>R$</span>
         <span id="total-value-products">
-          {ValueFormatter({ valor: state.amount })}
+          {ValueFormatter({ valor: inCart ? totalValue : state.amount })}
         </span>
       </div>
       <div id="remove-add-buttons-container">
