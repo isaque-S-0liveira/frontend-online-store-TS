69599ac9af67faf57e7ed93d8273a737869b345f
Isaque Oliveira <zazac3179@gmail.com>
2024-08-05 18:20:59 -0300

    fix: a função ValueFormatter não quebra mais a aplicação

--- src/services/ValueFormatter.ts
+++ src/services/ValueFormatter.ts
@@ -1,4 +1,7 @@
 export default function ValueFormatter({ valor }: { valor: number }): string {
+  if (typeof valor !== 'number' || Number.isNaN(valor)) {
+    return '0,00';
+  }
   let valorFormatado = valor.toFixed(2);
   valorFormatado = valorFormatado.replace('.', ',');
   valorFormatado = parseFloat(valorFormatado.replace(',', '.')).toLocaleString('pt-BR');
