6cbd9fbec8edb94ba0907476fbc0755491c10f24
Isaque Oliveira <zazac3179@gmail.com>
2024-08-05 18:15:01 -0300

    refactor: Exclusão da página de carrinho de compras

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import { Route, Routes } from 'react-router-dom';
 import Layout from './components/Layout';
 import Home from './pages/Home/Home';
 import ProductDetails from './pages/ProductDetails/ProductDetails';
-import ShoppingCart from './pages/ShoppingCart/ShoppingCart';
 
 function App() {
   return (
@@ -10,7 +9,6 @@ function App() {
       <Route path="/" element={ <Layout /> }>
         <Route index element={ <Home /> } />
         <Route path="product-details/:productId" element={ <ProductDetails /> } />
-        <Route path="shopping-cart" element={ <ShoppingCart /> } />
         <Route path="*" element={ <h1>Not Found</h1> } />
       </Route>
     </Routes>
--- src/pages/ShoppingCart/ShoppingCart.tsx
+++
@@ -1,9 +0,0 @@
-function ShoppingCart() {
-  return (
-    <main className="main-content">
-      <h1>O carrinho está vazio</h1>
-    </main>
-  );
-}
-
-export default ShoppingCart;
