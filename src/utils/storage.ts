270768adbc78c81c654a278b82bd2f56e85f3819
Isaque Oliveira <zazac3179@gmail.com>
2024-08-09 12:22:37 -0300

    feat: funções auxiliares para manipulação do localStorage

--- src/utils/storage.ts
+++ src/utils/storage.ts
@@ -1,14 +1,26 @@
 import { ProductCart } from '../types/ProductTypes';
+import { Review } from '../types/ReviewTypes';
 
 const PRODUCTS_KEY = 'products';
+const REVIEWS_KEY = 'reviews';
 
 export const getStoredProducts = (): ProductCart[] => {
   const storedProducts = localStorage.getItem(PRODUCTS_KEY);
   return storedProducts ? JSON.parse(storedProducts) : [] as ProductCart[];
 };
 
+export const getStoredReviews = (): Review[] => {
+  const storedReviews = localStorage.getItem(REVIEWS_KEY);
+  return storedReviews ? JSON.parse(storedReviews) : [] as Review[];
+};
+
 export const setStoredProducts = (products: ProductCart[]): void => {
   localStorage.setItem(PRODUCTS_KEY, JSON.stringify(products));
+  dispatchStorageUpdate();
+};
+
+export const setStoredReviews = (reviews: Review[]): void => {
+  localStorage.setItem(REVIEWS_KEY, JSON.stringify(reviews));
   dispatchStorageUpdate();
 };
 
