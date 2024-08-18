a8dfedbfadcff4aeede566ae638751f07631f3d2
Isaque Oliveira <zazac3179@gmail.com>
2024-08-18 16:48:31 -0300

    fix: productStock reload null

--- src/pages/ProductDetails/TechnicalSpecifications/TechnicalSpecifications.tsx
+++ src/pages/ProductDetails/TechnicalSpecifications/TechnicalSpecifications.tsx
@@ -1,24 +1,21 @@
-/* eslint-disable max-len */
-import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
-import AddCartButton from '../../../components/AddCartButton/AddCartButton';
-import AddRemoveProducts from '../../../components/AddRemoveTotalProducts/AddRemoveTotalProducts';
+import { useState, useEffect } from 'react';
 import { Product } from '../../../types/ProductTypes';
-import './TechnicalSpecifications.css';
+import AddRemoveTotalProducts from '../../../components/AddRemoveTotalProducts/AddRemoveTotalProducts';
+import AddCartButton from '../../../components/AddCartButton/AddCartButton';
 
 function TechnicalSpecifications({ productDetail }: { productDetail: Product }) {
   const location = useLocation();
-  const stockQuantity = location.state.productStock.available_quantity;
-  const [productStock, setProductStock] = useState({ available_quantity: 0 });
-  const [quantityAndValue, setQuantityAndValue] = useState({ quantity: 1, totalValue: productDetail.price });
+  const stockQuantity = location.state?.productStock?.available_quantity || 50;
+
+  const [productStock, setProductStock] = useState({ available_quantity: stockQuantity });
+
+  const [quantityAndValue, setQuantityAndValue] = useState({
+    quantity: 1, totalValue: productDetail.price });
 
   useEffect(() => {
-    if (stockQuantity) {
-      setProductStock({ available_quantity: stockQuantity });
-    } else {
-      setProductStock({ available_quantity: 50 });
-    }
-  }, []);
+    setProductStock({ available_quantity: stockQuantity });
+  }, [stockQuantity]);
 
   if (!productDetail || !productDetail.pictures) {
     return null;
@@ -45,23 +42,17 @@ function TechnicalSpecifications({ productDetail }: { productDetail: Product }) 
                 {' '}
                 {attribute.name}
                 :
-
-                {' '}
-                {' '}
               </strong>
               {attribute.values.map((value) => (
                 <span key={ value.name }>{value.name}</span>
               ))}
-
             </p>
           ))}
         </ul>
       </div>
       <div id="product-technical-specifications-footer" className="row p-0 m-0">
-        <div
-          className="col-12 col-sm-6 col-md-12 col-lg-12 col-xl-6 "
-        >
-          <AddRemoveProducts
+        <div className="col-12 col-sm-6 col-md-12 col-lg-12 col-xl-6">
+          <AddRemoveTotalProducts
             price={ productDetail.price }
             setQuantityAndValue={ setQuantityAndValue }
             productStock={ productStock.available_quantity }
@@ -82,7 +73,6 @@ function TechnicalSpecifications({ productDetail }: { productDetail: Product }) 
           />
         </div>
       </div>
-
     </div>
   );
 }
